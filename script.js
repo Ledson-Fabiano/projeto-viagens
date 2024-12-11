4b2c45e8ed467856caff6f11b4da02fabe41e3d2
Ledson-Fabiano <ledsonpedro5@gmail.com>
2024-12-11 14:39:23 -0300

    script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function menuShow() {
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
 }
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) 
+})
